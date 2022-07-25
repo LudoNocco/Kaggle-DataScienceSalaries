acafdfcfa3415e17589d455782941e6bee471320
LudoNocco <l.r.nocco@gmail.com>
2022-07-25 22:22:37 +0100

    Added linear regression analysis

--- data_exploration.py
+++ data_exploration.py
@@ -13,32 +13,64 @@ def save_data_graphs(dataframe_path: str):
     
     features = df.columns
     for feature in features:
-        if feature in ["work_year", "experience_level"]:
+        if feature in ["work_year", "experience_level", "employment_type"]:
             df[feature].value_counts().plot(kind="bar")
             plt.savefig(f"graphs/{feature}_barplot.png")
+            plt.close()
+        
+        elif feature == "job_title":
+            df[feature].value_counts().sort_values(ascending=True)[-5:].plot(kind="barh")
+            plt.savefig(f"graphs/{feature}_barplot.png")
+            plt.close()
+        
+            # Extracting unfrequent job titles
+            top_titles = ["head", "lead", "principal", "director"]
+            print(top_titles)
+            df["top_title"] = df.apply(lambda x: x["job_title"].lower().split(" ")[0] in top_titles, axis=1)
+            df_top_titles = df[df["top_title"]==True]
+            print("Number top level positions: ", df_top_titles.shape)
+            print("Top level position average salary: ", df_top_titles["salary"].mean())
 
-        elif feature == "experience_level":
+            low_titles = df[feature].value_counts().sort_values(ascending=True)[-5:].values
+            print(low_titles)
+            df["low_title"] = df.apply(lambda x: x["job_title"].lower().split(" ")[0] in low_titles, axis=1)
+            df_low_titles = df[df["low_title"]==True]
+            print("Number low level positions: ", df_low_titles.shape)
+            print("Low level position average salary: ", df_low_titles["salary"].mean())
+
             import ipdb
             ipdb.set_trace()
-        
-        elif feature == "employment_type":
-            pass
-        elif feature == "job_title":
-            pass
+
         elif feature == "salary":
+            #import ipdb
+            #ipdb.set_trace()
             pass
         elif feature == "salary_currency":
+            #import ipdb
+            #ipdb.set_trace()
             pass
         elif feature == "salary_in_usd":
+            #import ipdb
+            #ipdb.set_trace()
             pass
         elif feature == "employee_residence":
+            #import ipdb
+            #ipdb.set_trace()
             pass
         elif feature == "remote_ratio":
+            #import ipdb
+            #ipdb.set_trace()
             pass
         elif feature == "company_location":
+            #import ipdb
+            #ipdb.set_trace()
             pass
         elif feature == "company_size":
+            #import ipdb
+            #ipdb.set_trace()
             pass
+
+        
     
     return
 
--- run_analyses.py
+++ run_analyses.py
@@ -1,14 +1,78 @@
+from typing import List
+from itertools import combinations
+import os
+import numpy as np
 import pandas as pd
+ 
 from scipy.stats import spearmanr
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.metrics import mean_squared_error
 
-def analyse_data(dataframe_path:str):
+def run_analysis(dataframe_path: str):
+
+    assert os.path.isfile(dataframe_path), f"Invalid path for data: {dataframe_path}"
+
     df = pd.read_csv(data_path)
-    salary = df["salary"]
-    explevel = df["experience_level"]
 
-    correlation = spearmanr(salary, explevel)
-    print(correlation)
+    variables = df.columns
+    pairs = combinations(variables, 2)
+    pairs = [pair for pair in pairs]
+    
+    df_stats = pd.DataFrame(columns=["pair", "spearmann", "p_value"])
+    for ind, pair in enumerate(pairs):
+        variable1, variable2 = pair 
+
+        correlation = spearmanr(df[variable1], df[variable2])
+        corr, pvalue = correlation
+
+        df_stats.at[ind, "pair"] = variable1 + " & " + variable2
+        df_stats.at[ind, "spearmann"] = corr
+        df_stats.at[ind, "p_value"] = pvalue
+
+        df_significant = df_stats[df_stats["p_value"] <= 0.01]
+
+        df_significant.to_csv("results/significant_correlations.csv", index=False)
+
+    training_features = ["experience_level", "employment_type", "job_title"]
+    target_variable = "salary_in_usd" 
+
+    run_linear_regression(data=df, training_features=training_features, target_variable=target_variable)
+
+def run_linear_regression(data: pd.DataFrame, training_features: List, target_variable: str):
+
+    # Shuffle data
+    data = data.sample(frac = 1)
+
+    # Extract target and features
+    target = data[target_variable]
+    features = data[training_features]
+    
+    # Preprocess training data
+    features = pd.get_dummies(features)
+
+    # Get amount of trainning and test instances
+    tot_train = int(features.shape[0] * 0.8)
+    tot_test = int(features.shape[0] * 0.2)
+
+    # Sample training and test data
+    X_train = features.head(tot_train)
+    X_test = features.tail(tot_test)
+     
+    Y_train = target.head(tot_train)
+    Y_test = target.tail(tot_test)
+    
+    # Model training
+    model = LinearRegression()
+    model.fit(X_train.values, Y_train.values)
+
+    # Model evluation
+    predictions = model.predict(X_test.values)
+    mse = mean_squared_error(Y_test.values, predictions)
+    print(f"Mean Squared Error: {mse}")
+    print(predictions)
+    print(Y_test)
 
 if __name__=="__main__":
     data_path = "data/dsjs.csv"
-    analyse_data(data_path)
+    run_analysis(data_path)
